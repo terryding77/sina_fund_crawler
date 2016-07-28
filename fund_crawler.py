f18902b4530d89129166dd10baab36cd7016e34d
terryding77 <550147740@qq.com>
2016-07-28 23:57:28 +0800

    make fund_crawler to crawl all fund information in funds.csv

---
+++ fund_crawler.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import sys
+from urllib2 import urlopen
+import re
+import json
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+
+def log(str):
+    print(str)
+
+
+def get_url(url_base, arg_dict):
+    return "%s?%s" % (url_base, "&".join(["%s=%s" % (k, str(v)) for k, v in arg_dict.items()]))
+
+
+def get_data(url):
+    return urlopen(url).read()
+
+
+def crawler_all_fund():
+    url_base = 'http://vip.stock.finance.sina.com.cn/fund_center/data/jsonp.php/IO/NetValue_Service.getNetValueOpen'
+    args = {
+        'page': 0,
+        'num': 1000,
+        'sort': "symbol",
+        'asc': 0
+    }
+    url = get_url(url_base, args)
+    data = get_data(url)
+
+    total_num = int(re.findall("(?<=total_num:)\d*(?=,)", data)[0])
+    print(total_num)
+    page_size = args['num']
+    page_len = (total_num + page_size - 1) / page_size
+    funds = []
+    for i in range(page_len):
+        args['page'] = i + 1
+        data = get_data(get_url(url_base, args)).decode('gbk')
+        data = data.replace(',', ',"').replace('{', '{"').replace(':', '":').replace('},"', '},')
+        data = re.findall("(?<=IO\(\().*(?=\)\))", data)[0]
+        data = json.loads(data)
+        funds += data.get('data', [])
+
+    titles = sorted(list(set([k for l in funds for k in l])))
+    print(titles)
+    with open('funds.csv', 'w') as f:
+        f.write(", ".join(titles))
+        f.write('\n')
+        for l in funds:
+            print(", ".join(["%s = %s" % (k,v) for k, v in l.items()]))
+            f.write(", ".join([unicode(l.get(k, "")).encode("gbk") for k in titles]))
+            f.write('\n')
+
+
+if __name__ == '__main__':
+    print("start all fund crawler.")
+    crawler_all_fund()
